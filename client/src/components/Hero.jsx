af35cba5735bbcb4282966d5dcc5588e3a0cf05e
imsaurabhp <saurabhpatil95@gmail.com>
2023-12-16 20:32:14 +0530

    Initial Commit

---
+++ client/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+        <div className='bg-pink-600 w-full h-1/2'>
+Hi
+        </div>
+    </>
+  )
+}
+
+export default Hero
